caae6b6bfbd520c3380cf1a09d7b1d897f0fb7a6
tabing16 <tabing16@users.noreply.github.com>
2022-10-15 12:15:47 +0800

    Initial Commit

--- src/main/java/Jukebox1.java
+++ src/main/java/Jukebox1.java
@@ -1,7 +1,19 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MockSongs {
+public class Jukebox1{
+    public static void main(String[] args) {
+        new Jukebox1().go();
+    }
+
+    public void go(){
+        List<String> songList = MockSongs.getSongString();
+        System.out.println(songList);
+    }
+}
+
+class MockSongs {
     public static List<String> getSongString(){
         List<String> songs = new ArrayList<>();
         songs.add("somersault");
