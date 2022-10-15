a9a3ba9559116ce3ce06fbc691565b9746fa82a8
tabing16 <tabing16@users.noreply.github.com>
2022-10-15 12:24:48 +0800

    Initial Commit

--- src/main/java/Jukebox1.java
+++ src/main/java/Jukebox1.java
@@ -24,4 +24,39 @@ class MockSongs {
         songs.add("50 Ways");
         return songs;
     }
+
+    public static List<SongV2> getSongV2(){
+        List<SongV2> songs = new ArrayList<>();
+        songs.add(new SongV2("somersault","zero", 147));
+        songs.add(new SongV2("cassidy", "grateful dead", 158));
+        songs.add(new SongV2("$10", "hitchiker", 140));
+        songs.add(new SongV2("havana","cabello", 105));
+        songs.add(new SongV2("Cassidy", "grateful dead", 158));
+        songs.add(new SongV2("50 Ways", "simon", 102));
+        return songs;
+    }
+}
+
+class SongV2{
+    private String title;
+    private String artist;
+    private int bpm;
+
+    SongV2(String title, String artist, int bpm){
+        this.title = title;
+        this.artist = artist;
+        this.bpm = bpm;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getBpm() {
+        return bpm;
+    }
 }
